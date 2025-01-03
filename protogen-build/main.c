d61f352e2de500c9a025067736d05a4e99fc65eb
Henry Wysong-Grass <henryjwg@gmail.com>
2025-01-03 10:09:41 -0600

    Initial commit

---
+++ protogen-build/main.c
@@ -0,0 +1,81 @@
+/*******************************************************************************
+* File Name:   main.c
+*
+* Description: This is the source code for the Empty Application Example
+*              for ModusToolbox.
+*
+*******************************************************************************/
+
+/*******************************************************************************
+* Header Files
+*******************************************************************************/
+#if defined (CY_USING_HAL)
+#include "cyhal.h"
+#endif
+#include "cybsp.h"
+
+/******************************************************************************
+* Macros
+*******************************************************************************/
+
+
+/*******************************************************************************
+* Global Variables
+*******************************************************************************/
+
+
+/*******************************************************************************
+* Function Prototypes
+*******************************************************************************/
+
+
+/*******************************************************************************
+* Function Definitions
+*******************************************************************************/
+
+/*******************************************************************************
+* Function Name: main
+*********************************************************************************
+* Summary:
+* This is the main function for CPU. It...
+*    1.
+*    2.
+*
+* Parameters:
+*  void
+*
+* Return:
+*  int
+*
+*******************************************************************************/
+int main(void)
+{
+    cy_rslt_t result;
+
+#if defined (CY_DEVICE_SECURE) && defined (CY_USING_HAL)
+    cyhal_wdt_t wdt_obj;
+
+    /* Clear watchdog timer so that it doesn't trigger a reset */
+    result = cyhal_wdt_init(&wdt_obj, cyhal_wdt_get_max_timeout_ms());
+    CY_ASSERT(CY_RSLT_SUCCESS == result);
+    cyhal_wdt_free(&wdt_obj);
+#endif
+
+    /* Initialize the device and board peripherals */
+    result = cybsp_init();
+
+    /* Board init failed. Stop program execution */
+    if (result != CY_RSLT_SUCCESS)
+    {
+        CY_ASSERT(0);
+    }
+
+    /* Enable global interrupts */
+    __enable_irq();
+
+    for (;;)
+    {
+    }
+}
+
+/* [] END OF FILE */
